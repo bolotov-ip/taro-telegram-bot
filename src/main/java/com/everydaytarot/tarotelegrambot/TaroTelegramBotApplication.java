bc3753d44bbc919c9fd8418ae5802397bfe5f953
bolotov-ip <innpkenty.bolotov@gmail.com>
2022-09-28 11:00:32 +0800

    init project in git

---
+++ src/main/java/com/everydaytarot/tarotelegrambot/TaroTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.everydaytarot.tarotelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaroTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaroTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everydaytarot/tarotelegrambot/TaroTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everydaytarot.tarotelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaroTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
