e0f3c2c99e995e8d641ff7623de6114fded65433
bolotov-ip <innpkenty.bolotov@gmail.com>
2022-10-02 18:16:51 +0800

    jdbc settings

--- src/main/java/com/everydaytarot/tarotelegrambot/TaroTelegramBotApplication.java
+++ src/main/java/com/everydaytarot/tarotelegrambot/TaroTelegramBotApplication.java
@@ -3,12 +3,13 @@ package com.everydaytarot.tarotelegrambot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class TaroTelegramBotApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		SpringApplication.run(TaroTelegramBotApplication.class, args);
 		System.out.println("running");
 	}
-
 }
--- src/main/java/com/everydaytarot/tarotelegrambot/dao/ServiceDao.java
+++ src/main/java/com/everydaytarot/tarotelegrambot/dao/ServiceDao.java
@@ -1,4 +1,32 @@
 package com.everydaytarot.tarotelegrambot.dao;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+
+@Component
 public class ServiceDao {
+
+    @Value("${spring.datasource.url}")
+    private String urlDatabase;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    public void testConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection(urlDatabase, username, password);
+        String updatePositionSql = "select * from users";
+        try (PreparedStatement pstmt = connection.prepareStatement(updatePositionSql); ResultSet rs = pstmt.executeQuery();) {
+            while(rs.next()) {
+                String field = rs.getString(1);
+                System.out.println(field);
+            }
+        }
+        connection.close();
+    }
+
 }
